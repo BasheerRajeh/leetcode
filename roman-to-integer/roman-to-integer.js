640af3b2c3fd8b3f8c5bca64e7dd6fba643e6555
Basheer Rajeh <98283543+BasheerRajeh@users.noreply.github.com>
2023-05-22 23:36:33 +0300

    Time: 120 ms (85.17%) | Memory: 47.3 MB (59.33%) - LeetSync

--- roman-to-integer/roman-to-integer.js
+++ roman-to-integer/roman-to-integer.js
@@ -3,24 +3,19 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-  const simpleRules = {
+  const romanNum = {
     I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M:1000
   };
 
-  const complexRules = {
-    IV: 4, IX: 9, XL: 40, XC: 90, CD: 400, CM: 900
-  };
-
   let result = 0;
-  for(let index = 0; index< s.length; index++ ){
-    const sub = s.slice(index, index + 2);
-    if(complexRules[sub]){
-      result += complexRules[sub];
-      index++;
-    }else {
-      result += simpleRules[s[index]]; 
+
+  for(let i = 0; i < s.length - 1; i++){
+    if(romanNum[s[i]] < romanNum[s[i + 1]]){
+      result -= romanNum[s[i]];  
+    }else{
+      result += romanNum[s[i]]
     }
   }
-  return result;
 
+  return result + romanNum[s[s.length - 1]];
 };
