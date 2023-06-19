909933bf7f197f003275d917cf32a97721791d3f
Basheer Rajeh <98283543+BasheerRajeh@users.noreply.github.com>
2023-06-19 03:40:16 +0300

    Time: 59 ms (53.98%) | Memory: 41.9 MB (64.68%) - LeetSync

--- find-the-highest-altitude/find-the-highest-altitude.js
+++ find-the-highest-altitude/find-the-highest-altitude.js
@@ -4,7 +4,7 @@
  */
 var largestAltitude = function(gain) {
     let current = gain[0];
-    let maxAltitude = Math.max(current, 0);
+    let maxAltitude = current > 0? current: 0;
     for(let i = 1; i < gain.length; i++){
         current += gain[i];
         if(current > maxAltitude){
