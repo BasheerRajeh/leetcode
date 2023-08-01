b89c418d3b5068942be2f955858dbe8b7bf8a967
Basheer Rajeh <98283543+BasheerRajeh@users.noreply.github.com>
2023-08-01 03:39:30 +0300

    Time: 77 ms (100.00%) | Memory: 48.6 MB (35.13%) - LeetSync

---
+++ 77-combinations/combinations.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) {
+    const combinations = [];
+    const backtrack = (comArr, currentNo = 1)=>{
+        if(comArr.length === k ){
+            combinations.push([...comArr]);
+        }
+        if(comArr.length < k){
+            for(let i = currentNo; i <= n; i++ ){
+                comArr.push(i);
+                backtrack(comArr, i + 1);
+                comArr.pop();
+            }
+        }
+    }
+    backtrack([]);
+    return combinations;        
+}
