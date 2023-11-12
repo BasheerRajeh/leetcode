f9254b1260fc81313034b528a12aeb8526277534
Basheer Rajeh <98283543+BasheerRajeh@users.noreply.github.com>
2023-11-12 23:28:41 +0300

    Time: 62 ms (82.32%) | Memory: 44.9 MB (67.54%) - LeetSync

--- 47-permutations-ii/permutations-ii.js
+++ 47-permutations-ii/permutations-ii.js
@@ -3,26 +3,34 @@
  * @return {number[][]}
  */
 var permuteUnique = function(nums) {
-    let res = [];
-    dfs(nums.sort(), res, new Set());
-    return res;
-};
+    const res = []
+    const prems = []
+    const count = nums.reduce((acc, val)=>{
+        acc[val]? acc[val] += 1: acc[val] = 1
+        return acc
+    }, {})
 
-const dfs = (nums, res, visited) => {
-    if (nums.length === visited.size) {
-        let arr = [];
-        for (let idx of visited) {
-            arr.push(nums[idx]);
+    const dfs = () =>{
+        if(prems.length === nums.length){
+            res.push(prems.slice())
+            return
         }
-        res.push(arr);
-        return;
+
+        for(let n in count){
+            if(count[n] > 0){
+                prems.push(n)
+                count[n] -= 1
+
+                dfs()
+
+                count[n] += 1
+                prems.pop()
+            }
+        }
     }
+
+    dfs()
+    return res
     
-    for (let i = 0; i < nums.length; i++) {         
-        if (nums[i] === nums[i-1] && !visited.has(i-1)) continue;
-        if (visited.has(i)) continue;
-        visited.add(i);
-        dfs(nums, res, visited);
-        visited.delete(i);
-    }
-}
+
+};
