befbd690b4b8c23a04eae4330f99c45ca885edba
Basheer Rajeh <98283543+BasheerRajeh@users.noreply.github.com>
2023-11-29 23:57:51 +0300

    Time: 56 ms (49.30%) | Memory: 43.5 MB (70.99%) - LeetSync

---
+++ 257-binary-tree-paths/binary-tree-paths.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+    if(!root) return []
+
+    if(!root.left && !root.right) return [`${root.val}`]
+    else{
+        const left = binaryTreePaths(root.left).map(x => root.val + '->' + x)
+        const right = binaryTreePaths(root.right).map(x => root.val + '->' + x)
+
+        return [...left, ...right]
+    }
+}
