7115d3978ba80b33c3fbf6ed5e403281c3204f8b
Basheer Rajeh <98283543+BasheerRajeh@users.noreply.github.com>
2023-12-06 07:15:19 +0300

    Time: 118 ms (42.76%) | Memory: 47.9 MB (46.34%) - LeetSync

---
+++ 322-coin-change/coin-change.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    dp = Array(amount + 1).fill(Number.MAX_VALUE)
+    dp[0] = 0
+
+    for(let a = 1; a <= amount + 1; a++){
+        for(let c of coins) {
+            if(a >= c) {
+                dp[a] = Math.min(dp[a], 1 + dp[a - c])
+            }
+        }
+    }
+
+    return dp[amount] !== Number.MAX_VALUE? dp[amount] : -1 
+};
