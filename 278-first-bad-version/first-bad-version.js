8bc6564fe18bc79687873c598c40369c59f612a0
Basheer Rajeh <98283543+BasheerRajeh@users.noreply.github.com>
2023-12-14 05:21:33 +0300

    Time: 2697 ms (7.22%) | Memory: 41.6 MB (57.97%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let f = n
+        while(isBadVersion(n))
+        {
+            f = n
+            n --
+        }
+        return f
+    };
+};
