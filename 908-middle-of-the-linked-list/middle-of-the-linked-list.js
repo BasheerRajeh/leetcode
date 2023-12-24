bb148ef36ad0bd8b679b29d0dc13ba08cf880940
Basheer Rajeh <98283543+BasheerRajeh@users.noreply.github.com>
2023-12-24 19:52:07 +0300

    Time: 39 ms (98.11%) | Memory: 42 MB (33.04%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.js
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.js
@@ -10,15 +10,12 @@
  * @return {ListNode}
  */
 var middleNode = function(head) {
+    let slow = head, fast = head
     
-    let p1 = head
-    if(!p1.next) return p1
-    let p2 = p1.next
-
-    while(p2 && p2.next){
-        p1 = p1.next
-        p2 = p2.next.next
+    while(fast && fast.next) {
+        fast = fast.next.next
+        slow = slow.next
     }
 
-    return p2 ? p1.next : p1
+    return slow
 };
