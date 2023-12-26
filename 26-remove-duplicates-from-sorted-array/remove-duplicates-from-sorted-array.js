d04924a85b9bf561681e830b4ae51eecf1604cf7
Basheer Rajeh <98283543+BasheerRajeh@users.noreply.github.com>
2023-12-26 16:19:55 +0300

    Time: 65 ms (51.27%) | Memory: 44.3 MB (75.70%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let slow = 0, fast = 0
+    while(fast < nums.length) {
+        if(nums[fast] !== nums[slow]){
+            slow++
+            nums[slow] = nums[fast]
+        }
+        fast++
+    }
+
+    return slow + 1
+};
