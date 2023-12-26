2329fc3f688359a1557829a512aa4c62103e6988
Basheer Rajeh <98283543+BasheerRajeh@users.noreply.github.com>
2023-12-26 16:31:06 +0300

    Time: 53 ms (51.35%) | Memory: 42 MB (51.32%) - LeetSync

---
+++ 27-remove-element/remove-element.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let fast = 0, slow = 0
+    while(fast < nums.length) {
+        if(nums[fast] !== val) {
+            nums[slow++] = nums[fast]
+        }
+        fast++
+    }
+
+    return slow
+};
