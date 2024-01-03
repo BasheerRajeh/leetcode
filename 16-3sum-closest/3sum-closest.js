aa2273063d904d8862919bc52f891196f2b0c5a2
Basheer Rajeh <98283543+BasheerRajeh@users.noreply.github.com>
2024-01-03 21:32:12 +0400

    Time: 70 ms (53.70%) | Memory: 44.9 MB (7.01%) - LeetSync

--- 16-3sum-closest/3sum-closest.js
+++ 16-3sum-closest/3sum-closest.js
@@ -4,33 +4,29 @@
  * @return {number}
  */
 var threeSumClosest = function(nums, target) {
-    if (nums.length < 3) {
-        return 0;
-    }
-    nums.sort((a, b) =>  a - b)
-    let delta = Number.MAX_SAFE_INTEGER;
-    for (let i = 0; i < nums.length - 2; i++) {
-        let sum = nums[i] + twoSumClosest(nums, i + 1, target - nums[i]);
-        if (Math.abs(delta) > Math.abs(target - sum)) {
-            delta = target - sum;
-        }
-    }
-    return target - delta;
-};
+    if(nums.length < 3) return 0
 
-var twoSumClosest = function(nums, start, target) {
-    let left = start, right = nums.length - 1;
-    let delta = Number.MAX_SAFE_INTEGER;
-    while (left < right) {
-        let sum = nums[left] + nums[right];
-        if (Math.abs(delta) > Math.abs(target - sum)) {
-            delta = target - sum;
-        }
-        if (sum < target) {
-            left++;
-        } else {
-            right--;
+    nums.sort((a, b) => a - b)
+    let diff = Number.MAX_SAFE_INTEGER
+    for(let i = 0; i < nums.length - 2; i++) {
+        let sum = nums[i] + twoSumClosest(nums, i + 1, target - nums[i])
+        if(Math.abs(target - sum) < Math.abs(diff)) {
+            diff = target - sum
         }
     }
-    return target - delta;
-};
+    return target - diff
+}
+
+function twoSumClosest(nums, start, target) {
+    let left = start, right = nums.length - 1
+    let diff = Number.MAX_SAFE_INTEGER
+    while(left < right) {
+        let sum = nums[left] + nums[right]
+        if(Math.abs(target - sum) < Math.abs(diff)) {
+            diff = target - sum
+        }
+        if(sum > target) right--
+        else left++
+    }
+    return target - diff
+}
