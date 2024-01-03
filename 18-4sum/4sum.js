9a1ca7c1ef09219e627a62f63fb90a8555e68aa7
Basheer Rajeh <98283543+BasheerRajeh@users.noreply.github.com>
2024-01-04 02:39:26 +0400

    Time: 86 ms (32.59%) | Memory: 47.1 MB (20.24%) - LeetSync

--- 18-4sum/4sum.js
+++ 18-4sum/4sum.js
@@ -11,69 +11,32 @@ function fourSum(nums, target) {
 function nSum(nums, n, target, start = 0) {
     const size = nums.length
     const result = []
-    if(size < n || size < 2) return result
-    
-    if(n === 2) {
+    if (size < n || size < 2) return result
+
+    if (n === 2) {
         let left = start, right = size - 1
-        let curLeft = nums[left], curRight = nums[right]
-        while(left < right) {
+        while (left < right) {
             const sum = nums[left] + nums[right]
-            if(sum < target) {
-                while(left < right && nums[left] === curLeft) left++
-            }else if(num > target) {
-                while(left < right && nums[right] === curRight) right--
-            }else{
-                result.push([left, right])
-                while(left < right && nums[left] === curLeft) left++
-                while(left < right && nums[right] === curRight) right--
+            let curLeft = nums[left], curRight = nums[right]
+            if (sum < target) {
+                while (left < right && nums[left] === curLeft) left++
+            } else if (sum > target) {
+                while (left < right && nums[right] === curRight) right--
+            } else {
+                result.push([nums[left], nums[right]])
+                while (left < right && nums[left] === curLeft) left++
+                while (left < right && nums[right] === curRight) right--
             }
         }
-    }else {
-        for(let i = start; i < size; i++) {
+    } else {
+        for (let i = start; i < size; i++) {
             const subs = nSum(nums, n - 1, target - nums[i], i + 1)
-            for(let sub of subs) {
+            for (let sub of subs) {
                 sub.push(nums[i])
                 result.push(sub)
             }
-            while(i < size - 1 && nums[i] === nums[i + 1]) i++
+            while (i < size - 1 && nums[i] === nums[i + 1]) i++
         }
     }
     return result
 }
-
-var fourSum = function(nums, target) {
-  nums.sort((a, b) => a - b);
-  return nSumTarget(nums, 4, 0, target);
-};
-
-var nSumTarget = function(nums, n, start, target) {
-  var sz = nums.length;
-  var res = [];
-  if (n < 2 || sz < n) return res;
-  if (n === 2) {
-    var lo = start, hi = sz - 1;
-    while (lo < hi) {
-      var sum = nums[lo] + nums[hi];
-      var left = nums[lo], right = nums[hi];
-      if (sum < target) {
-        while (lo < hi && nums[lo] === left) lo++;
-      } else if (sum > target) {
-        while (lo < hi && nums[hi] === right) hi--;
-      } else {
-        res.push([left, right]);
-        while (lo < hi && nums[lo] === left) lo++;
-        while (lo < hi && nums[hi] === right) hi--;
-      }
-    }
-  } else {
-    for (var i = start; i < sz; i++) {
-      var sub = nSumTarget(nums, n - 1, i + 1, target - nums[i]);
-      for (var arr of sub) {
-        arr.push(nums[i]);
-        res.push(arr);
-      }
-      while (i < sz - 1 && nums[i] === nums[i + 1]) i++;
-    }
-  }
-  return res;
-};
