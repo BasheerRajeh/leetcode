e239908f43e646404c9341bcbc32d8ecca1c5427
Basheer Rajeh <98283543+BasheerRajeh@users.noreply.github.com>
2024-01-21 17:33:28 +0400

    Time: 2145 ms (10.05%) | Memory: 54.8 MB (5.08%) - LeetSync

--- 79-word-search/word-search.js
+++ 79-word-search/word-search.js
@@ -4,35 +4,32 @@
  * @return {boolean}
  */
 var exist = function(board, word) {
-    
-    const rows = board.length
-    const cols = board[0].length
+    const setPath = new Set()
 
-    const cells = new Set()
+    const outOfBoundry = (x, y) => (x < 0 || y < 0 || x >= board.length || y >= board[0].length)
 
-    const check = (r, c, i) => {
-        if(i === word.length) return true
-        
-        if( r >= rows 
-            || c >= cols 
-            || r < 0 
-            || c < 0 
-            || word[i] !== board[r][c]
-            || cells.has(`${r}-${c}`)
-            )
+    const backtrack = (x, y, i) => {
+        if(i === word.length) 
+            return true
+        if(outOfBoundry(x, y) || setPath.has(`${x},${y}`) || board[x][y] !== word[i]) {
             return false
+        }
+
+        setPath.add(`${x},${y}`)
         
-        cells.add(`${r}-${c}`)
-        const res = check(r+1, c, i+1) || check(r, c+1, i+1) || check(r-1, c, i+1) || check(r, c-1, i+1)
-        cells.delete(`${r}-${c}`)
+        const res = backtrack(x + 1, y, i + 1) || backtrack(x - 1, y, i + 1) || backtrack(x, y + 1, i + 1) || backtrack(x, y - 1, i + 1)
+
+        setPath.delete(`${x},${y}`)
+
         return res
     }
 
-    for(let i = 0; i < rows; i++){
-        for(let j = 0; j < cols; j++)
-            if(check(i, j, 0)) 
-                return true;
+    for(let i = 0; i < board.length; i++) {
+        for(let j = 0; j < board[0].length; j++) {
+            if(backtrack(i, j, 0)) return true
+        }
     }
 
-    return false;
+    return false
+
 };
